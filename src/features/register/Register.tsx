e20526c812bab53b8b6bbbb6a3e0d9e1f30d7754
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-15 03:42:43 -0400

    added registration form

---
+++ src/features/register/Register.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+const Register = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: { target: { name: any; value: any; }; }) => {
+    console.log(e.target.name);
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {};
+  return (
+    <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <h4>Register</h4>
+        {/* name */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            username
+          </label>
+          <input
+            type='text'
+            className='form-input'
+            id='username'
+            name='username'
+            value={user.username}
+            onChange={handleChange}
+          />
+        </div>
+        {/* email */}
+        <div className='form-row'>
+          <label htmlFor='email' className='form-label'>
+            Email
+          </label>
+          <input
+            type='email'
+            className='form-input'
+            id='email'
+            name='email'
+            value={user.email}
+            onChange={handleChange}
+          />
+        </div>
+        {/* email */}
+        <div className='form-row'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            className='form-input'
+            id='password'
+            name='password'
+            value={user.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Register;
