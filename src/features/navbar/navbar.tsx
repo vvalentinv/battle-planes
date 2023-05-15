48eb5573bd51b8a71f2c7afece60b1fcc736290d
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-15 05:18:36 -0400

    customized navbar links

--- src/features/navbar/navbar.tsx
+++ src/features/navbar/navbar.tsx
@@ -18,23 +18,35 @@ function Navbar() {
   }
 
   return (
-    <nav id="nav" className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a id="header1" className="navbar-brand" href="index.html">logo</a>
-        <a id="header2" className="navbar-brand" href="#">some menu item</a>
-        <a id="header3" className="navbar-brand" href="#" hidden>some hidden menu item</a>
-
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <nav className="navbar bg-light">
-          <div className="container-fluid">
-            <span id="welcome" className="navbar-brand"></span>
-            <a id="login-status" className="navbar-brand" href="#">Logout</a>
-          </div>
-        </nav>  
-      </div>
-    </nav>
+    <ul id="navbar" className="nav nav-pills nav-justified">
+        <li className="nav-item">
+          <a id="header1" className="nav-link" href="#">logo</a>
+        </li>
+        <li className="nav-item">
+          <a id="header2" className="nav-link" href="#">Some feature</a>
+        </li>
+        <li className="nav-item">
+          <a id="header3" className="nav-link active" href="#" hidden>some hidden feature</a>
+        </li>
+        <li className="nav-item">
+          <a id="header4" className="nav-link active" href="#" hidden>some hidden feature</a>
+        </li>
+        <li className="nav-item">
+          <a id="header5" className="nav-link active" href="#" hidden>some hidden feature</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Welcome username</a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Change email address</a></li>
+            <li><a className="dropdown-item" href="#">Change password</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a id="login-status" className="nav-link" href="#">Logout</a>
+        </li>
+ 
+    </ul>
+    
   )
 }
 
