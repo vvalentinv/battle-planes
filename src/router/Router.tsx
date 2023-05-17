e0e2764f0da1f454e4dfcc1c6c8bad08ae4c9dbc
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-17 06:25:09 -0400

    added login to router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,12 @@
 import { Register } from '../features/register'
+import { Login } from '../features/login'
 import { Route, Routes } from 'react-router-dom'
 
 function Router() {
   return (
     <Routes>
       <Route path="/register" element={<Register />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   )
 }
