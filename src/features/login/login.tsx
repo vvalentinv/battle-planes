a971594ce6049fe7ee5c30277177c4bb2f4197f4
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2023-05-17 06:22:59 -0400

    added Login feature

---
+++ src/features/login/login.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+const Login = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e: { target: { name: any; value: any; }; }) => {
+    console.log(e.target.name);
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {};
+  return (
+    <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <h4>Login</h4>
+        {/* username */}
+        <div className='form-row'>
+          <label htmlFor='username' className='form-label'>
+            username
+          </label>
+          <input
+            type='text'
+            className='form-input'
+            id='username'
+            name='username'
+            value={user.username}
+            onChange={handleChange}
+          />
+        </div>
+        {/* password */}
+        <div className='form-row'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            className='form-input'
+            id='password'
+            name='password'
+            value={user.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Login;
